f107d996c786487bea5d53a7f2c5a5c094340ce2
Jan Pal <formula.pieramohi@gmail.com>
2024-04-11 17:39:54 +0300

    init bot

--- functions/bot.js
+++ functions/bot.js
@@ -1,17 +1,51 @@
-const { Telegraf } = require("telegraf");
+const { Telegraf } = require("telegraf")
+const { message } = require('telegraf/filters')
+
 const bot = new Telegraf(process.env.BOT_TOKEN);
+const adminId = new Telegraf(process.env.ADMIN_ID);
 
 const messages = {
   "knihi": "Вітаю! Рады, што вы вырашылі далучыцца да нашай \"Кніжнай ініцыятывы\". Паўдзельнічаць у ёй вельмі проста."
     + "\n1. Даслаць у гэты чат фота вокладцы і зместу!\n2. Дачакацца адказу 😳😅",
-  "danat": "Каманда, што піша пра кнігі",
+  "danat": "Кнопка данат.\
+      Дзякуй, што вырашылі падтрымаць нас! Вашыя ахвяранні пойдуць на падтрымку ініцыятывы з кнігамі! 😳🤓\
+      Па нумару карткі Беларусбанк:\
+      9112 3800 2141 8894\
+      10/27\
+      IBAN:   BY94AKBB30140014619630070000",
   "suviaz": "Вітаю! Напішыце сваю прапанову і хутка мы адкажам! А пакуль што можаце націснуць іншыя кнопкі 😳😏",
 };
 
+async function forwardToAdmin(ctx) {
+  console.log("forward message to admin")
+  await ctx.forwardMessage(adminId)
+  await bot.telegram.sendMessage(adminId, "Рабіце адказ праз гэтае паведамленне\n\@" + ctx.update?.message?.from.id)
+}
+
 Object.keys(messages).forEach(function(command) {
-  console.log(`Received '${command}'' command`);
-  bot.command(command, async (ctx) => await ctx.reply(messages[command]));
+  console.log(`Setup '${command}'' command`);
+  bot.command(command, async (ctx)=> {
+    console.log("Received local '${command}'' command")
+    await ctx.reply(messages[command])
+    await forwardToAdmin(ctx);
+  })
 });
+
+bot.on(message(), async (ctx) => {
+  if (ctx.update?.message?.reply_to_message != null  && ("" + ctx.update?.message?.from?.id) === adminId) {
+    const originalMessage = ctx.update.message.reply_to_message
+    const startIndex = originalMessage.text.indexOf("@")
+    const replyUserId = originalMessage.text.substring(startIndex + 1)
+    console.log("Admin replies to user: " + replyUserId)
+    if (ctx.update?.message.text != null) {
+      await bot.telegram.sendMessage(replyUserId, ctx.update?.message.text)
+    } else {
+      await bot.telegram.sendSticker(replyUserId, ctx.update?.message.sticker.file_id)
+    }
+  } else if ("" + ctx.update?.message?.from.id !== adminId) {
+    await forwardToAdmin(ctx)
+  }
+})
 
 exports.handler = async event => {
   try {
